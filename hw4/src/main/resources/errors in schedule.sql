85906ee8bec07031a3c7181eacc87ce20fe6e753
ilya_piv <ilya.piv.by@gmail.com>
2023-03-16 13:10:47 +0300

    hw4 complete

---
+++ hw4/src/main/resources/errors in schedule.sql
@@ -0,0 +1,25 @@
+SELECT films1.date,
+	films1.name,
+	films1.time,
+    films1.duration,
+	films1.endtime,
+    films2.name,
+    films2.time
+FROM (SELECT  sch.id,
+		f.name,
+		sch.date, 
+        sch.time,
+        addtime(sch.time,SEC_TO_TIME(f.duration*60)) as 'endtime',
+        f.duration
+FROM cinema.schedule as sch
+	join cinema.film as f on sch.film_id=f.id
+order by sch.date, sch.time) as films1
+	JOIN (SELECT  sch.id,
+			f.name,
+			sch.date, 
+			sch.time
+	FROM cinema.schedule as sch
+		join cinema.film as f on sch.film_id=f.id
+	order by sch.date, sch.time) as films2 ON films1.date=films2.date
+    WHERE films1.endtime>films2.time AND films1.time<films2.time;
+   
---
+++ hw4/src/main/resources/sbory.sql
@@ -0,0 +1,40 @@
+SELECT  sales.name,
+		SUM(sales.sold),
+        SUM(sales.earned),
+        AVG(sales.sold),
+        AVG(sales.earned)
+FROM (SELECT 	ts.schedule_id,
+				f.name,
+				count(ts.place) AS sold,
+				count(ts.place)*pr.price AS earned
+		FROM cinema.tickets_sales AS ts
+			JOIN cinema.schedule AS sch ON ts.schedule_id=sch.id
+			JOIN cinema.pricelist AS pr ON ts.schedule_id=pr.schedule_id
+			JOIN cinema.film AS f ON sch.film_id=f.id
+		GROUP BY ts.schedule_id) AS sales
+GROUP BY sales.name
+ORDER BY SUM(sales.earned) DESC;	
+
+
+SELECT  sales.timezone,
+		SUM(sales.sold),
+        SUM(sales.earned)
+FROM (SELECT 	ts.schedule_id,
+				sch.time,
+				CASE 
+					WHEN sch.time>=time('09:00') AND sch.time<time('15:00')
+						THEN 'c 9 до 15'
+					WHEN sch.time>=time('15:00') AND sch.time<time('18:00')
+						THEN 'с 15 до 18'
+					WHEN sch.time>=time('18:00') AND sch.time<time('21:00')
+						THEN 'с 18 до 21'
+					ELSE 'с 21 до 00'
+				END AS timezone,
+				count(ts.place) AS sold,
+				count(ts.place)*pr.price AS earned
+		FROM cinema.tickets_sales AS ts
+			JOIN cinema.schedule AS sch ON ts.schedule_id=sch.id
+			JOIN cinema.pricelist AS pr ON ts.schedule_id=pr.schedule_id
+		GROUP BY ts.schedule_id) AS sales
+GROUP BY sales.timezone;
+    
